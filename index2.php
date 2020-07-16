ce67c39a6bf627ae22eaf59995e97d030c944cd4
David <davidselemg@gmail.com>
2020-07-16 02:27:12 -0500

    Se agrega link formUltrasonido.php

--- index2.php
+++ index2.php
@@ -157,7 +157,7 @@ $nombreTec = $user->nomAp($tec);
                         <br>
                         <a href="formLiquidos.php"><img src="img/index/liquidos_penetrantes.jpg" id="liquidos_penetrantes"  ></a>
                         <a href="formParticulas.php"><img src="img/index/particulas_magneticas.jpg" id="particulas_magneticas"></a><br>
-                        <a href="formUltrasonido.php"><img src="img/index/ultrasonido_aws.jpg" id="ultrasonido_aws"></a>
+                        <a href="formUltrasonido.php"><img src="./img/index/ultrasonido_aws.jpg" id="ultrasonido_aws"></a>
                         <a href="#"><img src="img/index/ultrasonido_aws.jpg" id="ultrasonido_aws"></a>
                         <a href="#"><img src="img/index/ultrasonido_aws.jpg" id="ultrasonido_aws"></a>
                     </div>
