fd7787db793175e444f85996cd589e52203ca313
David <davidselemg@gmail.com>
2020-07-20 20:30:27 -0500

    Se agrega el campo de Ultrasonido en la búsqueda NR

--- js/buscarReportes.js
+++ js/buscarReportes.js
@@ -84,7 +84,7 @@ $(document).ready(function () {
 
                 if(cTecnica == "Ultrasonido") {
                     for (i = 0; i < aJSON.length; i++) {
-                         cad += "<tr><td>" + aJSON[i].clContrato + "</td><td>" + aJSON[i].clContrato + "</td><td>" + aJSON[i].clNumReporte + "</td><td>" + aJSON[i].feTecnico + "</td><td><a href='formMostrarDurezaNR.php'><p id='link'>Ver<p/></button></td><td><a href='formActualizarDureza.php'><p id='link'>Actualizar</p></a></td></tr>";
+                         cad += "<tr><td>" + aJSON[i].clContrato + "</td><td>" + aJSON[i].clContrato + "</td><td>" + aJSON[i].clNumReporte + "</td><td>" + aJSON[i].feTecnico + "</td><td><a href='formMostrarUltrasonidoNR.php'><p id='link'>Ver<p/></button></td><td><a href='formActualizarUltrasonido.php'><p id='link'>Actualizar</p></a></td></tr>";
                     }
                 }//ifliq
                 $("#sinRegistros").css("display", "none");
